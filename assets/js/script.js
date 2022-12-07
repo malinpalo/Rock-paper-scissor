f95aa9470fe22679534a570fd7b008da7056b31a
malinpalo <malinpalo.mp@gmail.com>
2022-12-07 09:56:20 +0000

    Add style to Welcome page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,113 @@
-/**
- *create game variables for manipulating the DOM
- */
- let buttons = document.getElementsByTagName('button');
- let resultDisplay
- 
+/**Function to start the game*/
+const theGame = () => {
+    /**Declaring global Variables to update the scoreboard */
+    let playerScore = 0;
+    let compScore = 0;
+    /**The welcome page that starts the game*/
+    const startG = () => {
+        const startBtn = document.querySelector('.welcome button');
+        const welcomeScreen = document.querySelector('.welcome');
+        const gamePlay = document.querySelector('.game-play');
 
-let r = document.getElementById('r');
-let p = document.getElementById('p');
-let s = document.getElementById('s');
-
-
-
-/** Add event listener for buttons used in the main game, code executes after the DOM content has completely been loaded */
-document.addEventListener('DOMContentLoaded', function() {
-    
-    for (let button of buttons) {
-        button.addEventListener('click', function() {
-            let playerPlays = this.getAttribute('data-type');
-            startGame(playerPlays);
-            computerPlaysRandom()
-
+        startBtn.addEventListener('click', ()=> {
+            welcomeScreen.classList.add('fadeOut'); 
+            gamePlay.classList.add('fadeIn');
         });
-       
+    }; 
+    //**Functions that makes the game play*/
+    //*Declearing variables*//
+    const playGame = () => {
+        const choices = document.querySelectorAll('.choices button');
+        const playHand = document.querySelector('.player-wave');
+        const compHand = document.querySelector('.computer-wave');
+        const weapons = document.querySelectorAll('.weapons img');
+
+        weapons.forEach(weapon => {
+            weapon.addEventListener('animationend', function(){
+                this.style.animation = '';
+            });
+        })
+        //**Computers random choices*/
+        const compChoices = ['rock', 'paper', 'scissors'];
+        choices.forEach(choice => {
+            choice.addEventListener('click', function() {
+                const randomComp = Math.floor(Math.random() * 3);
+                const choiceComp = compChoices[randomComp]; 
+
+                setTimeout(() =>{ 
+                    compareWeapons(this.textContent.trim(), choiceComp);
+                    playHand.src = `../assets/images/${this.textContent.trim()}.webp`;
+                    compHand.src = `../assets/images/${choiceComp.trim()}.webp`;
+                }, 2000);
+
+                playHand.style.animation ='userShake 2s ease';
+                compHand.style.animation ='compShake 2s ease';
+           
+            });
+        });
+    };
+    /**Updates the scoreboard*/
+    const score = () =>{
+        const userScore = document.querySelector('.user-score p');
+        const computerScore = document.querySelector('.comp-score p');
+        userScore.textContent = playerScore;
+        computerScore.textContent = compScore;
     }
-}); 
+    /**Compares player and computer hands and increments the options by one for each round played */
+    const compareWeapons = (choicePlayer, choiceComp) =>{
+        const winLose = document.querySelector('.winlose');
 
-/**The Game, here is the possible results of the computers and plyers choises of play*/
-function startGame(playerPlays) {
-    let computerPlays = computerPlaysRandom();
-    if (playerPlays === 'rock' && computerPlays === 'paper') {
-        document.getElementById('result-message').innerHTML = "Buu, You lose!"
-    }
-    if (playerPlays === 'paper' && computerPlays === 'rock') {
-        document.getElementById('result-message').innerHTML = "Yeah, You win!"
-    }
-    if (playerPlays === 'scissors' && computerPlays === 'paper') {
-        document.getElementById('result-message').innerHTML = "Yeah, You win!"
-    }
-    if (playerPlays === 'scissors' && computerPlays === 'rock') {
-        document.getElementById('result-message').innerHTML = "Buu, You lose!"
-    }
-    if (playerPlays === 'paper' && computerPlays === 'rock') {
-        document.getElementById('result-message').innerHTML = "Buu, You lose!"
-    }
-    if (playerPlays === 'rock' && computerPlays === 'scissors') {
-        document.getElementById('result-message').innerHTML = "Yeah, You win!"
-    }
-    else if (playerPlays === computerPlays) {
-        document.getElementById('result-message').innerHTML = "Oh,It's a draw!"
-    }
-    resultDisplay = result;
+        if(choicePlayer === choiceComp){
+            winLose.textContent = 'Oh, it is a tie';
+            return;
+        };
 
-}
+        if(choicePlayer === 'rock'){
+            if(choiceComp === 'scissors'){
+                winLose.textContent = 'Yeay, you win!'
+                playerScore++;
+                score();
+                return;
+            }else{
+                winLose.textContent = 'Buuhu, you lost!';
+                compScore++;
+                score();
+                return;
+            };
+        };
+        
+        if(choicePlayer === 'paper'){
+            if(choiceComp === 'scissors'){
+                winLose.textContent = 'Buuhu, you lost!'
+                compScore++;
+                score();
+                return;
+            }else{
+                winLose.textContent = 'Yeay, you win!';
+                playerScore++;
+                score();
+                return;
+            };
+        };
 
-function computerPlaysRandom() {
-    let choices = ['rock', 'paper', 'scissors'];
-    let randomNumber = (Math.floor(Math.random() * 3)+1);
-    return choices[randomNumber]; 
-}
-
-function result() {
-
-}
-
-function incrementScore() {
-
-}
-
-function winningText() {
-
-}
-
-function lostText() {
-
-}
-
-function playAgain() {
-
-}
+        if(choicePlayer === 'scissors'){
+            if(choiceComp === 'rock'){
+                winLose.textContent = 'Buuhu, you lost!'
+                compScore++;
+                score();
+                return;
+            }else{
+                winLose.textContent = 'Yeay, you win!';
+                playerScore++;
+                score();
+                return;
+            }
+        }
+      }
+    
+    //**Calls the functions for the game */
+    startG();
+    playGame();
+};
+//**Calls the Game */
+theGame();
